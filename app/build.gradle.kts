4f9b11783b216481a82a3c47ba3a80b3cbc84fcc
rkorona <korona.starx@gmail.com>
2026-07-02 01:46:23 +0800

    G

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.ksp)
-    id("dev.vicart.compose.material.symbols")
 }
 
 android {
