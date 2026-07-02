09fa0adf110d617404eff59d5cc25917e80a2d1f
Rkorona <korona.starx@gmail.com>
2026-07-02 23:30:41 +0800

    uo

--- app/src/main/java/io/axiom/editor/ui/icons/editorscreen/Format.kt
+++ app/src/main/java/io/axiom/editor/ui/icons/editorscreen/Format.kt
@@ -1,6 +1,5 @@
 package io.axiom.editor.ui.icons.editorscreen
 
-
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathFillType
 import androidx.compose.ui.graphics.SolidColor
@@ -10,15 +9,16 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 import io.axiom.editor.ui.icons.AppIcons
+
 @Suppress("CheckReturnValue")
 public val AppIcons.Format: ImageVector
   get() {
-    if (_format_indent_increase != null) {
-      return _format_indent_increase!!
+    if (_format_paint != null) {
+      return _format_paint!!
     }
-    _format_indent_increase =
+    _format_paint =
       ImageVector.Builder(
-          name = "format_indent_increase",
+          name = "format_paint",
           defaultWidth = 24.dp,
           defaultHeight = 24.dp,
           viewportWidth = 24f,
@@ -36,81 +36,64 @@ public val AppIcons.Format: ImageVector
             strokeLineMiter = 1f,
             pathFillType = PathFillType.Companion.NonZero,
           ) {
-            moveTo(4f, 21f)
-            quadTo(3.58f, 21f, 3.29f, 20.71f)
-            quadTo(3f, 20.43f, 3f, 20f)
-            reflectiveQuadTo(3.29f, 19.29f)
-            reflectiveQuadTo(4f, 19f)
-            horizontalLineTo(20f)
+            moveTo(11f, 22f)
+            quadTo(10.18f, 22f, 9.59f, 21.41f)
+            reflectiveQuadTo(9f, 20f)
+            verticalLineTo(16f)
+            horizontalLineTo(6f)
+            quadTo(5.18f, 16f, 4.59f, 15.41f)
+            reflectiveQuadTo(4f, 14f)
+            verticalLineTo(7f)
+            quadTo(4f, 5.35f, 5.18f, 4.17f)
+            reflectiveQuadTo(8f, 3f)
+            horizontalLineTo(19f)
             quadToRelative(0.43f, 0f, 0.71f, 0.29f)
-            reflectiveQuadTo(21f, 20f)
-            reflectiveQuadToRelative(-0.29f, 0.71f)
-            reflectiveQuadTo(20f, 21f)
-            horizontalLineTo(4f)
+            reflectiveQuadTo(20f, 4f)
+            verticalLineTo(14f)
+            quadToRelative(0f, 0.82f, -0.59f, 1.41f)
+            reflectiveQuadTo(18f, 16f)
+            horizontalLineTo(15f)
+            verticalLineToRelative(4f)
+            quadToRelative(0f, 0.82f, -0.59f, 1.41f)
+            reflectiveQuadTo(13f, 22f)
+            horizontalLineTo(11f)
             close()
-            moveToRelative(8f, -4f)
-            quadToRelative(-0.42f, 0f, -0.71f, -0.29f)
-            quadTo(11f, 16.43f, 11f, 16f)
-            reflectiveQuadToRelative(0.29f, -0.71f)
-            reflectiveQuadTo(12f, 15f)
-            horizontalLineToRelative(8f)
-            quadToRelative(0.43f, 0f, 0.71f, 0.29f)
-            reflectiveQuadTo(21f, 16f)
-            reflectiveQuadToRelative(-0.29f, 0.71f)
-            reflectiveQuadTo(20f, 17f)
-            horizontalLineTo(12f)
-            close()
-            moveToRelative(0f, -4f)
-            quadToRelative(-0.42f, 0f, -0.71f, -0.29f)
-            quadTo(11f, 12.43f, 11f, 12f)
-            reflectiveQuadToRelative(0.29f, -0.71f)
-            reflectiveQuadTo(12f, 11f)
-            horizontalLineToRelative(8f)
-            quadToRelative(0.43f, 0f, 0.71f, 0.29f)
-            reflectiveQuadTo(21f, 12f)
-            reflectiveQuadToRelative(-0.29f, 0.71f)
-            reflectiveQuadTo(20f, 13f)
-            horizontalLineTo(12f)
-            close()
-            moveTo(12f, 9f)
-            quadTo(11.58f, 9f, 11.29f, 8.71f)
-            reflectiveQuadTo(11f, 8f)
-            quadTo(11f, 7.57f, 11.29f, 7.29f)
-            reflectiveQuadTo(12f, 7f)
-            horizontalLineToRelative(8f)
-            quadToRelative(0.43f, 0f, 0.71f, 0.29f)
-            reflectiveQuadTo(21f, 8f)
+            moveTo(6f, 10f)
+            horizontalLineTo(18f)
+            verticalLineTo(5f)
+            horizontalLineTo(17f)
+            verticalLineTo(8f)
             quadToRelative(0f, 0.42f, -0.29f, 0.71f)
-            reflectiveQuadTo(20f, 9f)
-            horizontalLineTo(12f)
+            reflectiveQuadTo(16f, 9f)
+            reflectiveQuadTo(15.29f, 8.71f)
+            reflectiveQuadTo(15f, 8f)
+            verticalLineTo(5f)
+            horizontalLineTo(14f)
+            verticalLineTo(6f)
+            quadToRelative(0f, 0.43f, -0.29f, 0.71f)
+            reflectiveQuadTo(13f, 7f)
+            reflectiveQuadTo(12.29f, 6.71f)
+            quadTo(12f, 6.43f, 12f, 6f)
+            verticalLineTo(5f)
+            horizontalLineTo(8f)
+            quadTo(7.18f, 5f, 6.59f, 5.59f)
+            quadTo(6f, 6.18f, 6f, 7f)
+            verticalLineToRelative(3f)
             close()
-            moveTo(4f, 5f)
-            quadTo(3.58f, 5f, 3.29f, 4.71f)
-            reflectiveQuadTo(3f, 4f)
-            quadTo(3f, 3.57f, 3.29f, 3.29f)
-            reflectiveQuadTo(4f, 3f)
-            horizontalLineTo(20f)
-            quadToRelative(0.43f, 0f, 0.71f, 0.29f)
-            reflectiveQuadTo(21f, 4f)
-            quadToRelative(0f, 0.42f, -0.29f, 0.71f)
-            reflectiveQuadTo(20f, 5f)
-            horizontalLineTo(4f)
+            moveToRelative(0f, 4f)
+            horizontalLineTo(18f)
+            verticalLineTo(12f)
+            horizontalLineTo(6f)
+            verticalLineToRelative(2f)
             close()
-            moveTo(3.85f, 15.15f)
-            quadTo(3.6f, 15.4f, 3.3f, 15.28f)
-            reflectiveQuadTo(3f, 14.8f)
-            verticalLineTo(9.2f)
-            quadTo(3f, 8.85f, 3.3f, 8.73f)
-            reflectiveQuadTo(3.85f, 8.85f)
-            lineToRelative(2.8f, 2.8f)
-            quadTo(6.8f, 11.8f, 6.8f, 12f)
-            reflectiveQuadTo(6.65f, 12.35f)
-            lineToRelative(-2.8f, 2.8f)
+            moveToRelative(0f, 0f)
+            verticalLineTo(12f)
+            verticalLineToRelative(2f)
             close()
           }
         }
         .build()
-    return _format_indent_increase!!
+    return _format_paint!!
   }
 
-private var _format_indent_increase: ImageVector? = null
+private var _format_paint: ImageVector? = null
