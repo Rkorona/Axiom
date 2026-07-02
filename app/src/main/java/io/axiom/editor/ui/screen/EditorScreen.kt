189dbea62a86ae4e7f4cf4bc17a678f874fbdc78
rkorona <korona.starx@gmail.com>
2026-07-02 08:02:11 +0800

    G

--- app/src/main/java/io/axiom/editor/ui/screen/EditorScreen.kt
+++ app/src/main/java/io/axiom/editor/ui/screen/EditorScreen.kt
@@ -33,7 +33,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-import dev.vicart.material.symbols.*
+import dev.vicart.compose.material.symbols.resources.FilledSymbol
+import dev.vicart.compose.material.symbols.resources.MaterialSymbols
 
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
