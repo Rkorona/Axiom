72c397f4a9b0d3b46af652288d8be6be625c0113
rkorona <korona.starx@gmail.com>
2026-07-02 22:39:45 +0800

    CS

--- app/src/main/java/io/axiom/editor/ui/icons/editorscreen/ArrowBack.kt
+++ app/src/main/java/io/axiom/editor/ui/icons/editorscreen/ArrowBack.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
-
+import io.axiom.editor.ui.icons.AppIcons
 @Suppress("CheckReturnValue")
 public val AppIcons.ArrowBack: ImageVector
   get() {
